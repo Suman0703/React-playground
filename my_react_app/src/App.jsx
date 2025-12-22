2882f5c2b083fffe3e35aa0d4f87e69345c3d364
Suman0703 <sumandevimehmi36@gamil.com>
2025-12-22 13:21:00 +0530

    internal working of react

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,10 +1,12 @@
 import Welcome from "./Welcome.jsx"
 
 function App() {
+  const page = "WEBPAGE";
   return(
     <div>
       <h1>Hello React 🚀</h1>
       <p>My React journey starts today</p>
+      <h1> WELCOME TO MY {page} </h1> 
        <Welcome />
     </div>
   );
