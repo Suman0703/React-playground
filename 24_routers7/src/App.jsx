3082e10903a1b0bad36b4b4d34af369e1cf9c0f0
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:18 +0530

    Added React Router setup with NavBar and routes

--- 24_routers7/src/App.jsx
+++ 24_routers7/src/App.jsx
@@ -1,22 +1,26 @@
-import { Route, Routes,Link } from "react-router"
+import { Route, Routes, Link } from "react-router"
 import Home from "./Home"
 import About from "./About"
+import Login from "./Login"
+import NavBar from "./Navbar"
 
 function App() {
 
   return (
     <div>
-      <Link to="/home">Home</Link>
-      <br /><br />
-       <Link to="/about">About</Link>
+      <NavBar />
       <h1>React Routers 7</h1>
+
       <Routes>
       <Route  path="/home" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="/login" element={<Login/>} />
+
 
       </Routes>
     </div>
   )
+
 }
 
 export default App
---
+++ 24_routers7/src/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+import'./Header.css'
+
+
+export default function NavBar() {
+    return (
+        <div className="Header">
+            <div>
+                <Link className="link"><h2>Logo</h2></Link>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <Link className="link" to="/home">Home</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <Link className="link" to="/login">Login</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <Link className="link" to="/about">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
