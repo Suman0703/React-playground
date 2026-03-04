26de3597795b053e59982313897544a2a75868d4
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:16 +0530

    Added simple derived state example using count and double value

---
+++ 22_hooks2/src/DerivedState.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  // Derived state 
+  const fullName = firstName + " " + lastName;
+
+  return (
+    <div>
+      <h2>Derived State Example</h2>
+
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+
+      <h3>Full Name: {fullName}</h3>
+    </div>
+  );
+}
+
+export default App;
