6ac4c40a861bf3aa36ed8a970d8ba54f53e02a00
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:12 +0530

    Hooks in React

--- react_hooks/src/App.jsx
+++ react_hooks/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useCallback } from "react"
 
-function App() {
+function EffectAndCallback() {
+
   const [count, setCount] = useState(0)
 
+  const logCount = useCallback(() => {
+    console.log("Current count:", count)
+  }, [count])
+
+  useEffect(() => {
+    logCount()
+  }, [logCount])
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
   )
 }
 
-export default App
+export default EffectAndCallback
