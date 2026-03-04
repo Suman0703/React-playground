044fd9df965e1b10323600a7a08682cb4608bae9
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:15 +0530

    Practice useRef hook for focusing input field

---
+++ 22_hooks2/src/App.jsx
@@ -0,0 +1,38 @@
+import { useRef } from 'react'
+import './App.css'
+
+
+//useRef HOOK in React
+// function App() {
+
+//   const input = useRef(null);
+
+//   const inputHandler = () => {
+//     input.current.focus();
+//     input.current.style.color='red';   
+//     input.current.placeholder="Enter Your Name"
+//   }
+
+//   return (
+//     <div>
+//       <h1>UseRef Hook</h1>
+
+//       <input 
+//         ref={input} 
+//         type="text" 
+//         placeholder="Enter Username" 
+//       />
+
+//       <br /><br />
+
+//       <button onClick={inputHandler}>
+//         Focus on Input
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
