d093b1479c461401c52e12f63857276ef99e778d
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:15 +0530

    Integrate Bootstrap in React application

--- 21_hooks/src/App.jsx
+++ 21_hooks/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Counter from './Counter';
+import { Button } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
+import BasicExample from './Nav';
 
 
 // useEffect Hook in React
@@ -30,25 +33,54 @@ import Counter from './Counter';
 
 // export default App;
 
+
+
+// Lifecycle of useEffect Hook
+// function App() {
+
+//   const [count, setcount] =useState(0)
+//   const [data, setdata] =useState(0)
+
+//   return (
+//      <div>
+//             <Counter data={data} />
+
+//             <button>Count is: {count}</button>
+//             <br /><br />
+//             <button onClick={() => setcount(count + 1)}>Increase Count</button>
+
+//             <br /><br />
+
+//             <button>Data is: {data}</button>
+//             <br /><br />
+//             <button onClick={() => setdata(data + 1)}>Increase Data</button>
+//         </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// Bootstrap In React
 function App() {
 
-  const [count, setcount] =useState(0)
-  const [data, setdata] =useState(0)
-
   return (
-     <div>
-            <Counter data={data} />
+    <div>
+      <BasicExample />
+      <h1>Add BOOTSTRAP in React</h1>
+      <Button>Okay</Button>
+      <br /> <br />
+      <Alert variant='dark'>Hello BootStrap</Alert>
+      <br />
+      <Button variant='success'>OK</Button>
+      <br /> <br />
+      <Button variant='warning' onClick={() => alert("BootStrap Button Clicked")}>Boot Button</Button>
+      <br /><br />
+      <button onClick={() => alert("BootStrap Button Clicked")}>Simple Button</button>
 
-            <button>Count is: {count}</button>
-            <br /><br />
-            <button onClick={() => setcount(count + 1)}>Increase Count</button>
-
-            <br /><br />
-
-            <button>Data is: {data}</button>
-            <br /><br />
-            <button onClick={() => setdata(data + 1)}>Increase Data</button>
-        </div>
+    </div>
   );
 }
 
