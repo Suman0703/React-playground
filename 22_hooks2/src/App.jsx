6930aa976f37147fbdcbf9fa29ef76e9de183a43
Suman0703 <sumandevimehmi36@gmail.com>
2026-03-04 19:09:16 +0530

    Implemented passing function as prop from parent to child component

--- 22_hooks2/src/App.jsx
+++ 22_hooks2/src/App.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react'
 import './App.css'
+import Child from './Child';
 
 
 //useRef HOOK in React
@@ -38,74 +39,93 @@ import './App.css'
 
 
 // Uncontrolled Component in React 
+// function App() {
+
+//   const userRef = useRef(null);
+//   const passwordRef = useRef(null);
+
+//   const handleForm = (e) => {
+//     e.preventDefault();
+
+//     const user = e.target.user.value;
+//     const password = e.target.password.value;
+
+//     console.log("User:", user);
+//     console.log("Password:", password);
+//   };
+
+//   const handleFormRef = (e) => {
+//     e.preventDefault();
+
+//     console.log("User:", userRef.current.value);
+//     console.log("Password:", passwordRef.current.value);
+//   };
+
+//   return (
+//     <div>
+//       <h1>UnControlled Components</h1>
+
+//       <form method="post" onSubmit={handleForm}>
+//         <input
+//           type="text"
+//           name="user"
+//           placeholder="enter user name"
+//         />
+//         <br /><br />
+
+//         <input
+//           type="password"
+//           name="password"
+//           placeholder="enter user password"
+//         />
+//         <br /><br />
+
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       <hr />
+
+//       <h1>Uncontrolled Component with useRef</h1>
+
+//       <form method="post" onSubmit={handleFormRef}>
+//         <input
+//           type="text"
+//           ref={userRef}
+//           placeholder="enter user name"
+//         />
+//         <br /><br />
+
+//         <input
+//           type="password"
+//           ref={passwordRef}
+//           placeholder="enter user password"
+//         />
+//         <br /><br />
+
+//         <button type="submit">Submit</button>
+//       </form>
+
+//     </div>
+//   );
+// }
+
+// export default App
+
+
+
+// Passing Function as A prop
 function App() {
 
-  const userRef = useRef(null);
-  const passwordRef = useRef(null);
-
-  const handleForm = (e) => {
-    e.preventDefault();
-
-    const user = e.target.user.value;
-    const password = e.target.password.value;
-
-    console.log("User:", user);
-    console.log("Password:", password);
-  };
-
-  const handleFormRef = (e) => {
-    e.preventDefault();
-
-    console.log("User:", userRef.current.value);
-    console.log("Password:", passwordRef.current.value);
-  };
+  function handleClick() {
+    alert("Button clicked in Child component!");
+  }
 
   return (
     <div>
-      <h1>UnControlled Components</h1>
-
-      <form method="post" onSubmit={handleForm}>
-        <input
-          type="text"
-          name="user"
-          placeholder="enter user name"
-        />
-        <br /><br />
-
-        <input
-          type="password"
-          name="password"
-          placeholder="enter user password"
-        />
-        <br /><br />
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <hr />
-
-      <h1>Uncontrolled Component with useRef</h1>
-
-      <form method="post" onSubmit={handleFormRef}>
-        <input
-          type="text"
-          ref={userRef}
-          placeholder="enter user name"
-        />
-        <br /><br />
-
-        <input
-          type="password"
-          ref={passwordRef}
-          placeholder="enter user password"
-        />
-        <br /><br />
-
-        <button type="submit">Submit</button>
-      </form>
-
+      <h1>Parent Component</h1>
+      <Child onButtonClick={handleClick} />
     </div>
   );
 }
 
-export default App
+export default App;
